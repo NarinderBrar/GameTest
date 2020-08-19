193b7f89c34fb090ba96958badd01749fce44126
ChitkaraARVRLab <narinder.singh@chitkara.edu.in>
2020-08-19 13:52:36 +0530

    backup

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1597824725,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/florobj-sheet0.png",
+		"images/box-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
